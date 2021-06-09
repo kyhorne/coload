5d86911cf6b05519771e7ca6e16510cad5ea094d
kyhorne <me@kyhorne.com>
2021-06-09 08:56:00 -0400

    low resolution hero image

--- pages/index.tsx
+++ pages/index.tsx
@@ -11,7 +11,7 @@ export default function Home() {
         <main>
           <div
             style={{
-              background: "url(pokemon.jpg)",
+              background: "url(pokemon.png)",
               backgroundRepeat: "no-repeat",
               // backgroundAttachment: "fixed",
               backgroundPosition: "center",
