ff5b28d5b5b3992d044d4566877da4bc84e8617e
kyhorne <me@kyhorne.com>
2021-06-10 00:02:35 -0400

    added auth0

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,13 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
+import { UserProvider } from "@auth0/nextjs-auth0";
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
-}
-export default MyApp
+const MyApp = ({ Component, pageProps }: AppProps) => {
+  return (
+    <UserProvider>
+      <Component {...pageProps} />
+    </UserProvider>
+  );
+};
+
+export default MyApp;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,12 @@
 import Head from "next/head";
+import { useUser } from "@auth0/nextjs-auth0";
 
 export default function Home() {
+  const { user, error, isLoading } = useUser();
+
+  if (isLoading) return <div>Loading...</div>;
+  if (error) return <div>{error.message}</div>;
+
   return (
     <div>
       <Head>
@@ -29,7 +35,11 @@ export default function Home() {
                       <a href="#tutorial">How Coload Works</a>
                     </li>
                     <li>
-                      <a href="">Login</a>
+                      {user ? (
+                        <a href="/api/auth/logout">Logout</a>
+                      ) : (
+                        <a href="/api/auth/login">Login</a>
+                      )}
                     </li>
                     <li>
                       <a href="mailto:help@thecoload.com">Contact</a>
