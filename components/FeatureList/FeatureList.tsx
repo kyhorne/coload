679126c1341066b9c7104d598a7afae52f401026
kyhorne <me@kyhorne.com>
2021-06-13 23:50:06 -0400

    removed console log

--- components/FeatureList/FeatureList.tsx
+++ components/FeatureList/FeatureList.tsx
@@ -5,40 +5,35 @@ interface FeatureListProps {
   featuresRef: React.RefObject<HTMLDivElement>;
 }
 
-const FeatureList: React.FC<FeatureListProps> = ({ featuresRef }) => {
-  console.log(featuresRef);
-  return (
-    <section ref={featuresRef} className="features">
-      <div className="container">
-        <h3 className="text-center">
-          Goodbye, overpriced safety deposit boxes.
-        </h3>
-        <div className="grid my-4">
-          <FeatureCard
-            title={'Storage'}
-            image={'icons8-trolley'}
-            description={
-              'We offer subscription-based, personalized storage options for raw, sealed and slabbed trading card products.'
-            }
-          />
-          <FeatureCard
-            title={'Secure'}
-            image={'icons8-favorites-shield'}
-            description={
-              'Our 24/7 surveilled, light and temperature-controlled facility protects from theft, fires, floods, and natural disasters.'
-            }
-          />
-          <FeatureCard
-            title={'Marketplace (Coming Soon)'}
-            image={'icons8-price-tag'}
-            description={
-              'Infrastructure to buy or sell without the overhead of finding a merchant, storing, or shipping the product.'
-            }
-          />
-        </div>
+const FeatureList: React.FC<FeatureListProps> = ({ featuresRef }) => (
+  <section ref={featuresRef} className="features">
+    <div className="container">
+      <h3 className="text-center">Goodbye, overpriced safety deposit boxes.</h3>
+      <div className="grid my-4">
+        <FeatureCard
+          title={'Storage'}
+          image={'icons8-trolley'}
+          description={
+            'We offer subscription-based, personalized storage options for raw, sealed and slabbed trading card products.'
+          }
+        />
+        <FeatureCard
+          title={'Secure'}
+          image={'icons8-favorites-shield'}
+          description={
+            'Our 24/7 surveilled, light and temperature-controlled facility protects from theft, fires, floods, and natural disasters.'
+          }
+        />
+        <FeatureCard
+          title={'Marketplace (Coming Soon)'}
+          image={'icons8-price-tag'}
+          description={
+            'Infrastructure to buy or sell without the overhead of finding a merchant, storing, or shipping the product.'
+          }
+        />
       </div>
-    </section>
-  );
-};
+    </div>
+  </section>
+);
 
 export default FeatureList;
