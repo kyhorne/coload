50be7b60955f8374bb2120f54c52566d208f0feb
kyhorne <me@kyhorne.com>
2021-06-20 22:38:23 -0400

    removed amplify

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,10 +2,6 @@ import React from 'react';
 import '../styles/globals.scss';
 import type { AppProps } from 'next/app';
 import { UserProvider } from '@auth0/nextjs-auth0';
-import config from '../src/aws-exports';
-import Amplify from 'aws-amplify';
-
-Amplify.configure({ ...config, ssr: true });
 
 const MyApp = ({ Component, pageProps }: AppProps) => (
   <UserProvider>
