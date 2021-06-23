6d3409448ab2119e23a53ea1bc417dcc75663897
kyhorne <me@kyhorne.com>
2021-06-22 20:48:06 -0400

    removed console.log

--- components/SubscriptionForm/SubscriptionForm.tsx
+++ components/SubscriptionForm/SubscriptionForm.tsx
@@ -40,7 +40,6 @@ const SubscriptionForm: React.FC<SubscribeFormProps> = ({
     handleSubmit,
     didSubmit,
   ] = useSubscriptionForm(initialValues, async (items: Cart) => {
-    console.log(items);
     // create a checkout session
     const res = await fetchPostJSON('/api/create-checkout-session', items);
 
